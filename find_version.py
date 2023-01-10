9a78107b3974bbd91e61962a55fa739388d3e068
JiaYiChe <59781205+JiaYiChe@users.noreply.github.com>
2023-01-09 18:17:33 -0700

    Update find_version.py

--- find_version.py
+++ find_version.py
@@ -3,3 +3,5 @@ import requests
 print(requests.__version__)
 //print google home page's information
 print(requests.get("http://www.google.com").text)
+
+print(requests.get("https://raw.githubusercontent.com/JiaYiChe/404lab1/main/find_version.py").text)
